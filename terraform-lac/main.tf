7ef775925bf0a98a2a98a77782f6e51f7f12d568
manikumar <pmkumar458@gmail.com>
2024-11-19 11:38:30 +0530

    modification code

--- terraform-lac/main.tf
+++ terraform-lac/main.tf
@@ -6,11 +6,23 @@ resource "aws_vpc" "eks_vpc" {
   cidr_block = "10.0.0.0/16"
   enable_dns_support = true
   enable_dns_hostnames = true
+  tags = {
+    Name = "eks-vpc"
+  }
+}
+
+resource "aws_subnet" "eks_subnet" {
+  count = 2
+  vpc_id = aws_vpc.eks_vpc.id
+  cidr_block = cidrsubnet(aws_vpc.eks_vpc.cidr_block, 8, count.index)
+  map_public_ip_on_launch = true
+  availability_zone = data.aws_availability_zones.available.names[count.index]
 }
 
 resource "aws_eks_cluster" "eks_cluster" {
-  name = "web-app-cluster"
+  name     = "web-app-cluster"
   role_arn = aws_iam_role.eks_role.arn
+
   vpc_config {
     subnet_ids = aws_subnet.eks_subnet[*].id
   }
